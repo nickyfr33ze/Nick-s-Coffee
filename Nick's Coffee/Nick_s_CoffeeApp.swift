48a9ff623b0dea2a0c28042d7a0daf9f49553e5a
Nick Friesen <nfriesen26@gmail.com>
2023-12-06 17:06:17 -0800

    Initial Commit

---
+++ Nick's Coffee/Nick_s_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nick_s_CoffeeApp.swift
+//  Nick's Coffee
+//
+//  Created by Nick Friesen on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Nick_s_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
